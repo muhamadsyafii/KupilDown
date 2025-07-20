d1ff90952c711a1f3368d29d97f7dedc82380441
Muhamad Syafii <muhamadsyafii4@gmail.com>
2025-07-20 16:02:19 +0700

    initial project

---
+++ kupildown/src/main/java/dev/kupil/kupildown/KupilDownloader.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by Muhamad Syafii
+ * 20/7/2025 - muhamadsyafii4@gmail.com
+ * Copyright (c) 2025.
+ * All Rights Reserved
+ */
+
+package dev.kupil.kupildown
+
+class KupilDownloader {
+
+}
